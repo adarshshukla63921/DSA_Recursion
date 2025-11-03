4fcf5a09e45241134103157934faab950fd19c3c
adarshshukla63921 <adarshshukla63921@gmail.com>
2025-11-03 12:59:23 +0530

    n_sum

--- src/RecursionBasics.java
+++ src/RecursionBasics.java
@@ -5,7 +5,14 @@ public class RecursionBasics {
         num=num-1;
         namePrinter(name,num);
     }
+    public static int n_sum(int n, int sum){
+        if(n<=0) return sum;
+        sum+=n;
+        n=n-1;
+        return n_sum(n,sum);
+    }
     public static void main(String[] args) {
-        namePrinter("Adarsh",5);
+        namePrinter("Adarsh",1);
+        System.out.println(n_sum(5,0));
     }
 }
