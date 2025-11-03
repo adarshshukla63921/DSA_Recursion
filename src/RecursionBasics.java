8afd29199b529edf0eedb95a82d8f036c9ebd424
adarshshukla63921 <adarshshukla63921@gmail.com>
2025-11-03 13:24:49 +0530

    palindrome

--- src/RecursionBasics.java
+++ src/RecursionBasics.java
@@ -5,14 +5,34 @@ public class RecursionBasics {
         num=num-1;
         namePrinter(name,num);
     }
+
     public static int n_sum(int n, int sum){
         if(n<=0) return sum;
         sum+=n;
         n=n-1;
         return n_sum(n,sum);
     }
+
+    public static int fact(int n){
+        if(n<=1) return 1;
+        return n*fact(n-1);
+    }
+
+     public static int sumOfArray(int[] arr, int N, int idx){
+        if(N<idx) return 0;
+        return  arr[idx]+sumOfArray(arr,N,idx+1);
+     }
+     public static boolean checkPalindrome(String s){
+        return checkPalindromeHelper(s, 0, s.length()-1);
+     }
+
+     public static boolean checkPalindromeHelper(String s, int start, int end){
+        if(start>end) return true;
+        if(s.charAt(start)!=s.charAt(end)) return false;
+        return checkPalindromeHelper(s,start+1,end-1);
+     }
     public static void main(String[] args) {
-        namePrinter("Adarsh",1);
-        System.out.println(n_sum(5,0));
+       String str ="ab";
+       System.out.println(checkPalindrome(str));
     }
 }
